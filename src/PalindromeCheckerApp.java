0d7c1a2d92361ef70e74f807788f6d4d4d72b7b5
adhithya-jp <adhijp0007@gmail.com>
2026-03-13 15:15:48 +0530

    UC1:Application Entry & Welcome Message

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,8 @@
 public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        String appName = "PalindromeChecker App";
+        String version = "Version 1.0";
+        System.out.println(appName);
+        System.out.println(version);
+    }
 }
