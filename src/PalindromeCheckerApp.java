c021d709a615505090695f8329a9d0aa79a2d955
adhithya-jp <adhijp0007@gmail.com>
2026-03-13 15:31:00 +0530

    UC5:Stack-Based Palindrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,23 +1,21 @@
 import java.util.Scanner;
+import java.util.Stack;
 
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
-        char[] arr = input.toCharArray();
-        int left = 0;
-        int right = arr.length - 1;
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < input.length(); i++) {
+            stack.push(input.charAt(i));
+        }
         boolean palindrome = true;
-
-        while (left < right) {
-            if (arr[left] != arr[right]) {
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) != stack.pop()) {
                 palindrome = false;
                 break;
             }
-            left++;
-            right--;
         }
-
         if (palindrome) {
             System.out.println("Palindrome");
         } else {
